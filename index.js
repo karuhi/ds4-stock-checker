ba778301380430edd825a46163a9f398c8a9f061
かる <karuhi.dev@gmail.com>
2022-06-18 21:08:57 +0900

    perf: エラーハンドリングを消してみる
    動作が不安定すぎる

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
 import puppeteer from 'puppeteer'
-import { urls } from './items.js'
-
+import { items } from './items.js'
 ;(async () => {
     const browser = await puppeteer.launch({
         headless: false,
         args: ['--no-sandbox', '--lang=ja'],
     })
     await Promise.all(
-        urls.map((url) => getStockStateByColor(browser, url))
+        items.map((item) => getStockStateByColor(browser, item))
     ).then(() => console.log('取得完了'))
 
     browser.close()
 })()
 
-const getStockStateByColor = async (browser, url) => {
-    console.log(`${url.color}取得開始`)
+const getStockStateByColor = async (browser, item) => {
+    console.log(`${item.color}取得開始`)
     const page = await browser.newPage()
-    await page.goto(url.url)
+    await page.goto(item.url)
     await page
         .waitForSelector(
             'div.mainbox:nth-child(1) > dl:nth-child(2) > dt:nth-child(1)'
         )
         .then(async (element) => {
-            var text = await page
-                .evaluate((element) => element.textContent, element)
-                .catch((err) => {
-                    reject('evaluate err!!')
-                })
+            var text = await page.evaluate(
+                (element) => element.textContent,
+                element
+            )
             console.log('===========')
-            console.log(`${url.color}取得完了✨`)
+            console.log(`${item.color}取得完了✨`)
             console.log(text.replace(/\s+/g, ''))
             console.log('===========')
-            resolve('succes!!')
-        })
-        .catch((err) => {
-            return 'err!!'
         })
     page.close()
 }
