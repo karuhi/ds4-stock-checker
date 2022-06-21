d3f1aeef724e8b6573a37891105e00c4ab62f0a6
かる <karuhi.dev@gmail.com>
2022-06-21 19:17:56 +0900

    feat: 並列処理を完全実装＆エラーハンドリングの再実装 #1  #2 #3
    エラー落ちしてもブラウザは閉じるようにした。（プログラムが最後まで動く！）

--- index.js
+++ index.js
@@ -1,34 +1,82 @@
 import puppeteer from 'puppeteer'
-import { items } from './items.js'
-;(async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--no-sandbox', '--lang=ja'],
-    })
-    await Promise.all(
-        items.map((item) => getStockStateByColor(browser, item))
-    ).then(() => console.log('取得完了'))
+import { items, company } from './items.js'
+const parallelCount = 4
 
-    browser.close()
-})()
+const getControllerStocks = async (items, parallelCount) => {
+    const parallelBatches = Math.ceil(items.length / parallelCount)
 
-const getStockStateByColor = async (browser, item) => {
-    console.log(`${item.color}取得開始`)
-    const page = await browser.newPage()
-    await page.goto(item.url)
-    await page
-        .waitForSelector(
-            'div.mainbox:nth-child(1) > dl:nth-child(2) > dt:nth-child(1)'
-        )
-        .then(async (element) => {
-            var text = await page.evaluate(
-                (element) => element.textContent,
-                element
-            )
-            console.log('===========')
-            console.log(`${item.color}取得完了✨`)
-            console.log(text.replace(/\s+/g, ''))
-            console.log('===========')
+    console.log(`itemCount: ${items.length}, ParallelCount: ${parallelCount}`)
+
+    console.log(`Batches: ${parallelBatches}`)
+
+    // Itemsの配列を並列で処理する
+    let k = 0
+    for (let i = 0; i < items.length; i += parallelCount) {
+        k++
+        console.log('\nBatch ' + k + ' of ' + parallelBatches)
+        // Chromiumを起動する
+        const browser = await puppeteer.launch({
+            headless: false,
+            args: ['--no-sandbox', '--lang=ja'],
         })
-    page.close()
+        // puppeteer.launch()で起動したChromiumを使用する
+        const context = await browser.createIncognitoBrowserContext()
+        const page = await context.newPage()
+        page.setJavaScriptEnabled(false)
+
+        const promises = []
+        for (let j = 0; j < parallelCount; j++) {
+            const elem = i + j
+            // elements[elem]が存在するか確認する
+            if (items[elem] !== undefined) {
+                // スクレイピングをPromiseする
+                console.log(`🖖 ${items[elem].name.ja} Cheking...`)
+                promises.push(
+                    browser.newPage().then(async (page) => {
+                        await page.setViewport({ width: 1280, height: 800 })
+                        try {
+                            // page.gotoがエラーにならない場合のみ実行する
+                            await page.goto(items[elem].url)
+                            await page
+                                .waitForSelector(
+                                    items[elem].name.company == 0
+                                        ? 'div.mainbox:nth-child(1) > dl:nth-child(2) > dt:nth-child(1)'
+                                        : '#js_buyBoxMain > ul:nth-child(1) > li:nth-child(1) > div:nth-child(1) > p:nth-child(1)',
+                                    { timeout: 3000 }
+                                )
+                                .then(async (element) => {
+                                    var text = await page.evaluate(
+                                        (element) => element.textContent,
+                                        element
+                                    )
+                                    console.log(
+                                        `✅ ${items[elem].name.ja} is Successfly Checked.`
+                                    )
+                                    console.log(
+                                        `[${
+                                            company[items[elem].name.company]
+                                                .name
+                                        }] ${text.replace(/\s+/g, '')}`
+                                    )
+                                })
+                        } catch (err) {
+                            console.log(
+                                `❌ ${items[elem].name.ja} is Failed to Check.`
+                            )
+                        } finally {
+                            await page.close()
+                        }
+                    })
+                )
+            }
+        }
+
+        // すべてのPromiseの終了を待ってからブラウザを終了する
+        await Promise.all(promises)
+        await browser.close()
+
+        console.log('\nCheck has All ended.\nwaiting for next batch...\n')
+    }
 }
+
+getControllerStocks(items, parallelCount)
--- items.js
+++ items.js
@@ -1,26 +1,45 @@
+// DualShock4 コントローラーのリスト
 export const items = [
     {
-        color: 'black',
+        name: { en: 'Jett Black', ja: 'ジェット・ブラック', company: 0 },
         url: 'https://joshinweb.jp/game/26171/4948872414203.html',
     },
     {
-        color: 'white',
+        name: { en: 'Glacier White', ja: 'グレイシャー・ホワイト', company: 0 },
         url: 'https://joshinweb.jp/game/26171/4948872414340.html',
     },
     {
-        color: 'red',
+        name: { en: 'Magma Red', ja: 'マグマ・レッド', company: 0 },
         url: 'https://joshinweb.jp/game/26171/4948872414296.html',
     },
     {
-        color: 'blue',
+        name: { en: 'Midnight Blue', ja: 'ミッドナイト・ブルー', company: 0 },
         url: 'https://joshinweb.jp/game/26171/4948872414562.html',
     },
-    // {
-    //     color: 'gold',
-    //     url: 'https://joshinweb.jp/game/26171/4948872414357.html',
-    // },
-    // {
-    //     color: 'cooper',
-    //     url: 'https://joshinweb.jp/game/26171/4948872414753.html',
-    // },
+    {
+        name: { en: 'Gold', ja: 'ゴールド', company: 0 },
+        url: 'https://joshinweb.jp/game/26171/4948872414357.html',
+    },
+    {
+        name: { en: 'Cooper', ja: 'カッパー', company: 0 },
+        url: 'https://joshinweb.jp/game/26171/4948872414753.html',
+    },
+    {
+        name: { en: 'Jett Black', ja: 'ジェットブラック', company: 1 },
+        url: 'https://www.yodobashi.com/product/100000001003242957/',
+    },
+    {
+        name: { en: 'Glacier White', ja: 'グレイシャー・ホワイト', company: 1 },
+        url: 'https://www.yodobashi.com/product/100000001003420364/',
+    },
+    {
+        name: { en: 'Magma Red', ja: 'マグマ・レッド', company: 1 },
+        url: 'https://www.yodobashi.com/product/100000001003244511/',
+    },
+]
+
+// 企業名を使うためだけのアレ
+export const company = [
+    { name: 'joshinweb', url: 'https://joshinweb.jp/' },
+    { name: 'yodobashi', url: 'https://www.yodobashi.com/' },
 ]
